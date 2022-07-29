cbda17f9476bd0012e14ba8cf4eee9e52483f9ab
VladimsKov <vladimskv@yandex.ru>
2022-07-29 21:53:54 +0300

    modules in browser

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// TODO: write your code here
+import Game, {
+    GameSavingData,
+    readGameSaving as loadGame,
+    writeGameSaving as saveGame
+} from './game.js'
+
+
+const game = new Game();
+game.start();
+
+
+
+
+
+
+import sum from './basic.js';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
